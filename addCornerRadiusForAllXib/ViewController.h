9f9c67ce191202511e1f57335aa000c5afe582f6
huangshaohua <huangshaohua@xywy.com>
2015-12-28 16:47:40 +0800

    Initial Commit

---
+++ addCornerRadiusForAllXib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  addCornerRadiusForAllXib
+//
+//  Created by 黄少华 on 15/12/28.
+//  Copyright © 2015年 黄少华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
